20c6808d85b04b5052d76cfae309b984d2895b7a
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2022-11-08 10:54:47 +0100

    commit

--- 00_TrasmissioneSerialeArduino/fase2/codiceRicezione/codiceRicezione.ino
+++ 00_TrasmissioneSerialeArduino/fase2/codiceRicezione/codiceRicezione.ino
@@ -9,11 +9,13 @@ void loop()
   if (digitalRead(13) == LOW)
   {
     delayMicroseconds(1250);
+    char c = 0;
     for (int i = 0; i < 8; i++)
     {
       int var = digitalRead(13);
-      Serial.print(var);
+      bitWrite(c, i, var);
       delayMicroseconds(833);
     }
+    Serial.print(c);
   }
 }
