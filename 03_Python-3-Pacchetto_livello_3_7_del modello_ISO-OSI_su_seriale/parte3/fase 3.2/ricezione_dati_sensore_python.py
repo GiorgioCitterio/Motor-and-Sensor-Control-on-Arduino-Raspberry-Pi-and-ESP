617b5410510e9feee040990aae961ee5575a5bd3
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2022-11-17 11:53:15 +0100

    Update ricezione_dati_sensore_python.py

--- 03_Python-3-Pacchetto_livello_3_7_del modello_ISO-OSI_su_seriale/parte3/fase 3.2/ricezione_dati_sensore_python.py
+++ 03_Python-3-Pacchetto_livello_3_7_del modello_ISO-OSI_su_seriale/parte3/fase 3.2/ricezione_dati_sensore_python.py
@@ -1,10 +1,22 @@
 import serial;
 import time;
 import struct;
+IDCORRETTO = "BE"
 
 arduino = serial.Serial('COM3', 9600)
 
 print('inizio invio dei dati')
 while True:
     val = arduino.read(32)
-    print(val)
+    pack = struct.unpack("2s 4s 4s 2s 4s 16s", val)
+    id=pack[0].decode()
+    mittente=pack[1]
+    destinatario=pack[2]
+    tipo=pack[3]
+    valoreSensore=pack[4]
+    vuoto=pack[5]
+    if id==IDCORRETTO:
+        print("id corretto")
+    else:
+        print("pacchetto scartato")
+    
