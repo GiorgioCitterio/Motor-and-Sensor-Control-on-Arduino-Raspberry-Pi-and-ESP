d111ace17c6a22c316524acb2734882243c8c555
umbecol <91663374+umbecol@users.noreply.github.com>
2022-11-17 11:22:49 +0100

    Update sketch_arduino_sensore.ino

--- 03_Python-3-Pacchetto_livello_3_7_del modello_ISO-OSI_su_seriale/parte3/sketch_arduino_sensore/sketch_arduino_sensore.ino
+++ 03_Python-3-Pacchetto_livello_3_7_del modello_ISO-OSI_su_seriale/parte3/sketch_arduino_sensore/sketch_arduino_sensore.ino
@@ -1,6 +1,8 @@
 #define ID "BE"
 #define MITTENTE "M001"
 #define DESTINATARIO "D031"
+#define TIPO "S1"
+#define VUOTO "----------------"
 
 struct pacchettoS1 {
     char id[2];
@@ -8,7 +10,7 @@ struct pacchettoS1 {
     char destinatario[4];
     char tipo[2];
     char valoreSensore[4];
-    char vuoto[16]
+    char vuoto[16];
   };
 
 void setup()
@@ -16,18 +18,20 @@ void setup()
   Serial.begin(9600);
 }
 void loop()
-{
-  
-  struct pacchettoS1 msg;
-  memcpy(msg.id, ID, sizeof(msg.id);
+{  
   int num = analogRead(A0);
   char s[5];
   sprintf(s, "%04d", num);
+  
+  struct pacchettoS1 msg;
+  memcpy(msg.id, ID, sizeof(msg.id));
+  memcpy(msg.mittente, MITTENTE, sizeof(msg.mittente));
+  memcpy(msg.destinatario, DESTINATARIO, sizeof(msg.destinatario));
+  memcpy(msg.tipo, TIPO, sizeof(msg.tipo));
+  memcpy(msg.valoreSensore, s, sizeof(msg.valoreSensore));
+  memcpy(msg.vuoto, VUOTO, sizeof(msg.vuoto));
+  
   Serial.write((byte *)&msg, sizeof(msg));
-  delay(1000);
-  if (Serial.available())
-  {
-    
-  }
-  }
+  
+  delay(10000);
 }
