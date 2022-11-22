a7f78332f1a53266fd43beb973054d5f29819656
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2022-11-22 12:56:51 +0100

    Update sketch_motore.ino

--- 03_Python-3-Pacchetto_livello_3_7_del modello_ISO-OSI_su_seriale/parte3/fase 3.4/sketch_motore/sketch_motore.ino
+++ 03_Python-3-Pacchetto_livello_3_7_del modello_ISO-OSI_su_seriale/parte3/fase 3.4/sketch_motore/sketch_motore.ino
@@ -1,3 +1,14 @@
+
+struct pacchettoA1 {
+  char id[2];
+  char mittente[4];
+  char destinatario[4];
+  char tipo[2];
+  char direzione[1];
+  char velocita[3];
+  char vuoto[16];
+};
+
 void setup()
 {
   Serial.begin(9600);
