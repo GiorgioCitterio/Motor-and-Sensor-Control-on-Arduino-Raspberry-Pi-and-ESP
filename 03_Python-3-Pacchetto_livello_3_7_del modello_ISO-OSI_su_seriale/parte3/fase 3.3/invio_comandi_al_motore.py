954ec0f0e22dabf91d844fb01cc9b4dad9f1f93e
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2022-11-22 12:18:50 +0100

    Update invio_comandi_al_motore.py

--- 03_Python-3-Pacchetto_livello_3_7_del modello_ISO-OSI_su_seriale/parte3/fase 3.3/invio_comandi_al_motore.py
+++ 03_Python-3-Pacchetto_livello_3_7_del modello_ISO-OSI_su_seriale/parte3/fase 3.3/invio_comandi_al_motore.py
@@ -6,14 +6,14 @@ DESTINATARIO=b"D031"
 TIPO=b"A1"
 VUOTO=b"----------------"
 
-arduino = serial.Serial('COM3', 9600)
+#arduino = serial.Serial('COM3', 9600)
 
 print('inizio invio dei dati')
 while True:
-    d = input("direzione ")
-    DIREZIONE = str(d).zfill(3).encode()
-    v = int(input("velocita "))
-    VELOCITA=str(v).zfill(1).encode()
-    pack=struct.pack("2s 4s 4s 2s 3s 1s 16s",ID,MITTENTE,DESTINATARIO, DIREZIONE, VELOCITA, TIPO, VUOTO)
-    arduino.write(pack)
+    d = input("direzione a/i: ")
+    DIREZIONE = str(d).zfill(1).encode()
+    v = input("velocità: ")
+    VELOCITA=str(v).zfill(3).encode()
+    pack=struct.pack("2s 4s 4s 2s 1s 3s 16s",ID,MITTENTE,DESTINATARIO, TIPO, DIREZIONE, VELOCITA, VUOTO)
+    #arduino.write(pack)
     print(pack)
