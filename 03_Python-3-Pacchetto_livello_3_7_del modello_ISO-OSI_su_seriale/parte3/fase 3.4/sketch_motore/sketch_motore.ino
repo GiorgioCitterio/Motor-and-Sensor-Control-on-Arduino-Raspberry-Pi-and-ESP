9ebc8e976421678895354c15df56c8b7a3cecfc4
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2022-11-24 08:59:13 +0100

    Update sketch_motore.ino

--- 03_Python-3-Pacchetto_livello_3_7_del modello_ISO-OSI_su_seriale/parte3/fase 3.4/sketch_motore/sketch_motore.ino
+++ 03_Python-3-Pacchetto_livello_3_7_del modello_ISO-OSI_su_seriale/parte3/fase 3.4/sketch_motore/sketch_motore.ino
@@ -1,7 +1,8 @@
 #define ID "BE"
 #define DESTINATARIO "D031"
 
-struct pacchettoA1 {
+struct pacchettoA1
+{
   char id[2];
   char mittente[4];
   char destinatario[4];
@@ -14,11 +15,37 @@ struct pacchettoA1 {
 void setup()
 {
   Serial.begin(9600);
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
 }
+
 void loop()
-{  
+{
+  char vel[4];
   struct pacchettoA1 msg;
-  if (Serial.available()){
-    byte msg = Serial.readBytes((byte *) &msg, sizeof(msg));
+  if (Serial.available())
+  {
+    Serial.readBytes((byte*) &msg, sizeof(msg));
+    int controlloId = memcmp(ID, msg.id, 2);
+    int controlloDest = memcmp(DESTINATARIO, msg.destinatario, 4);
+    if (controlloId == 0 && controlloDest == 0)
+    {
+      strcpy(vel, msg.velocita);
+      int velocita = atoi(vel);
+      String direzione = (String)msg.direzione;
+      if (direzione == "a")
+      {
+        digitalWrite(3, LOW);
+        digitalWrite(5, HIGH);
+        digitalWrite(9, velocita);
+      }
+
+      if (direzione == "i")
+      {
+        digitalWrite(3, HIGH);
+        digitalWrite(5, LOW);
+        digitalWrite(9, velocita);
+      }
+    }
   }
 }
