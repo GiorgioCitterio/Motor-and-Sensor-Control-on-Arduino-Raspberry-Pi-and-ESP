8fda88438f2964d7ad818c90cb1b2ab977c42144
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2022-12-01 14:21:26 +0100

    Update guiAttuatore_2.py

--- 04_Python-4-GUI/parte2/soluzione2/guiAttuatore_2.py
+++ 04_Python-4-GUI/parte2/soluzione2/guiAttuatore_2.py
@@ -1,39 +1,29 @@
 import tkinter as tk
 import serial
-import struct
-ID=b"BE"
-MITTENTE=b"M001"
-DESTINATARIO=b"D031"
-TIPO=b"A1"
-VUOTO=b"----------------"
 direzione = b"A"
 velocita = 0
 
 arduino = serial.Serial('COM3', 9600)
 window = tk.Tk() 
-window.geometry("265x100")
+window.geometry("250x100")
 window.title("Controllo motore")
 window.resizable(False, False)
 
 def DirezioneAvanti():
     global direzione
     direzione = b"A"
-    v=str(velocita).zfill(3).encode()
-    pack=struct.pack("2s 4s 4s 2s 1s 3s 16s",ID,MITTENTE,DESTINATARIO, TIPO, direzione, v, VUOTO)
-    arduino.write(pack)
+    arduino.write(direzione)
 
 def DirezioneIndietro():
     global direzione
     direzione = b"I"
-    v=str(velocita).zfill(3).encode()
-    pack=struct.pack("2s 4s 4s 2s 1s 3s 16s",ID,MITTENTE,DESTINATARIO, TIPO, direzione, v, VUOTO)
-    arduino.write(pack)
+    arduino.write(direzione)
 
 avantiDir_button = tk.Button(text="Avanti", command=DirezioneAvanti)
-avantiDir_button.grid(row=1, column=0, sticky="W")
+avantiDir_button.grid(row=0, column=0, sticky="W")
 
 indietroDir_button = tk.Button(text="Indietro", command=DirezioneIndietro)
-indietroDir_button.grid(row=1, column=1, sticky="E")
+indietroDir_button.grid(row=0, column=2, sticky="E")
 
 if __name__ == "__main__":
     window.mainloop()
