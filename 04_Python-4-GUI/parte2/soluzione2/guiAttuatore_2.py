696220c3b1442f2e6ba452af2c2715079703f0b7
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2022-12-01 15:06:31 +0100

    aggiornamentoGenerale

--- 04_Python-4-GUI/parte2/soluzione2/guiAttuatore_2.py
+++ 04_Python-4-GUI/parte2/soluzione2/guiAttuatore_2.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 import serial
-direzione = b"A"
-velocita = 0
+direzione = "avanti"
 
 arduino = serial.Serial('COM3', 9600)
 window = tk.Tk() 
@@ -11,13 +10,19 @@ window.resizable(False, False)
 
 def DirezioneAvanti():
     global direzione
-    direzione = b"A"
-    arduino.write(direzione)
+    direzione = "A"
+    entry1String = entry1.get()
+    stringaDaInviare = "{0};{1}".format(direzione,entry1String).encode()
+    print(stringaDaInviare)
+    arduino.write(stringaDaInviare)
 
 def DirezioneIndietro():
     global direzione
-    direzione = b"I"
-    arduino.write(direzione)
+    direzione = "I"
+    entry1String = entry1.get()
+    stringaDaInviare = "{0};{1}".format(direzione,entry1String).encode()
+    print(stringaDaInviare)
+    arduino.write(stringaDaInviare)
 
 avantiDir_button = tk.Button(text="Avanti", command=DirezioneAvanti)
 avantiDir_button.grid(row=0, column=0)
@@ -30,6 +35,7 @@ velocitaText_Label.grid(row=1, column=0, pady=5)
 
 entry1 = tk.Entry(window, width=20)
 entry1.grid(row=1, column=1)
+
 
 if __name__ == "__main__":
     window.mainloop()
