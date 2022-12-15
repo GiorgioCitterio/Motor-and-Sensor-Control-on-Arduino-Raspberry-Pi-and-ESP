8075ff2f82a68872ae9b4c89e92aae2423cec2bd
umbecol <91663374+umbecol@users.noreply.github.com>
2022-12-15 16:40:43 +0100

    commit

--- 05_Python-5-JSON/parte2/esercizioDiz2.py
+++ 05_Python-5-JSON/parte2/esercizioDiz2.py
@@ -1,14 +1,21 @@
 import json
 lista = []
+lista2 = []
 
 for i in range(7):
-    dataOra = input("inserire la data e l'ora: ")
-    val = int(input("inserire il valore: "))
-    dizionario = {'DataOra': dataOra, 'Valore' : val}
+    #dataOra = input("inserire la data e l'ora: ")
+    #val = int(input("inserire il valore: "))
+    #dizionario = {'DataOra': dataOra, 'Valore' : val}
+    dizionario = {'DataOra': i+1, 'Valore' : i+1}
     lista.append(dizionario)
 
 print("\n")
 data = json.dumps(lista)
-file = open('05_Python-5-JSON/parte2/esDiz.json', 'a')
+file = open('05_Python-5-JSON/parte2/esDiz.json', 'w')
 file.write(data)
 file.close()
+
+with open('05_Python-5-JSON/parte2/esDiz.json', 'r') as fp:
+    lista2 = json.load(fp)
+
+print(lista2[-5:])
---
+++ 05_Python-5-JSON/parte3/esercizioDiz3.py
@@ -0,0 +1,40 @@
+import serial
+import struct
+import json
+import time
+IDCORRETTO = "BE"
+DESTINATARIOCORRETTO = "D031"
+
+arduino = serial.Serial('COM3', 9600)
+
+lista = []
+cont = 0
+
+while True:
+    val = arduino.read(32)
+    pack = struct.unpack("2s 4s 4s 2s 4s 16s", val)
+    print(pack)
+    id=pack[0].decode()
+    mittente=pack[1].decode()
+    destinatario=pack[2].decode()
+    tipo=pack[3].decode()
+    valoreSensore=pack[4].decode()
+    vuoto=pack[5].decode()
+    if (id==IDCORRETTO)and(destinatario==DESTINATARIOCORRETTO):
+        print("id e destinatario corretti e valoreSensore = " + valoreSensore)
+        s=int(valoreSensore)
+        dataOra = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+        dizionario = {'DataOra': dataOra, 'Valore' : s}
+        lista.append(dizionario)
+        cont += 1
+    else:
+        print("pacchetto scartato")  
+    
+    if cont>10:
+        data = json.dumps(lista[-10:])
+        with open('05_Python-5-JSON/parte3/esDiz.json', 'w') as fp:
+            fp.write(data)
+        with open('05_Python-5-JSON/parte3/esDiz.json', 'r') as fp:
+            lista2 = json.load(fp)
+        print(lista2) 
+        time.sleep(30)
