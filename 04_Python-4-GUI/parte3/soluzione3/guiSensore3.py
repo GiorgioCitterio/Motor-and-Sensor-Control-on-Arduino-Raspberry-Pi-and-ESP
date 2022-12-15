7e258a0628c1db741762ac3dfe8160e07e172a4f
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2022-12-15 08:57:18 +0100

    commit guiSensore3 finito

--- 04_Python-4-GUI/parte3/soluzione3/guiSensore3.py
+++ 04_Python-4-GUI/parte3/soluzione3/guiSensore3.py
@@ -6,9 +6,10 @@ DESTINATARIOCORRETTO = "D031"
 
 arduino = serial.Serial('COM3', 9600)
 y = []
+#y1 = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100]
 x = []
 
-for i in range(30):
+for i in range(20):
     val = arduino.read(32)
     pack = struct.unpack("2s 4s 4s 2s 4s 16s", val)
     print(pack)
@@ -20,14 +21,14 @@ for i in range(30):
     vuoto=pack[5].decode()
     if (id==IDCORRETTO)and(destinatario==DESTINATARIOCORRETTO):
         print("id e destinatario corretti, valore del sensore = "+valoreSensore)
-        y.append(valoreSensore)
+        y.append(int(valoreSensore))
         x.append(i)
     else:
         print("pacchetto scartato")
 
-y.sort()
-plt.plot(x, y, color = 'red')
+plt.plot(x, y, marker="o",color = 'red')
 plt.title("Il grafico dei valori del sensore")
 plt.xlabel("X - Secondi")
 plt.ylabel("Y - Valori sensore")
+plt.axes([0, 15, 0, 1023])
 plt.show()
