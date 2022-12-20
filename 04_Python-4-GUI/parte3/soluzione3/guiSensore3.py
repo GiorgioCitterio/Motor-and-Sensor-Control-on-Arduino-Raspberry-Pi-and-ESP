1c73b600cea14e0a7b1811a7af4b00e337f97f8d
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2022-12-20 18:30:41 +0100

    Update guiSensore3.py

--- 04_Python-4-GUI/parte3/soluzione3/guiSensore3.py
+++ 04_Python-4-GUI/parte3/soluzione3/guiSensore3.py
@@ -2,6 +2,7 @@ import serial
 import struct
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import time
 IDCORRETTO = "BE"
 DESTINATARIOCORRETTO = "D031"
 
@@ -11,22 +12,7 @@ ax = fig.add_subplot(1, 1, 1)
 xs = []
 ys = []
 
-def GrafFunz(i, xs, ys):
-    xs.append(i)
-    ys.append(valoreSensore)
-    xs = xs[-20:]
-    ys = ys[-20:]
-    ax.clear()
-    ax.plot(xs, ys)
-    plt.xticks(rotation=45, ha='right')
-    plt.subplots_adjust(bottom=0.30)
-    plt.title("Il grafico dei valori del sensore")
-    plt.xlabel("X - Secondi")
-    plt.ylabel("Y - Valori sensore")
-    plt.axes([0, i, 0, 1023])
-    return 0
-
-for i in range(20):
+def animate(i, xs, ys):
     val = arduino.read(32)
     pack = struct.unpack("2s 4s 4s 2s 4s 16s", val)
     print(pack)
@@ -38,9 +24,20 @@ for i in range(20):
     vuoto=pack[5].decode()
     if (id==IDCORRETTO)and(destinatario==DESTINATARIOCORRETTO):
         print("id e destinatario corretti, valore del sensore = "+valoreSensore)
-        ani = animation.FuncAnimation(fig, GrafFunz, fargs=(xs, ys), interval=1000)
-        plt.show()
-        ani.save()
+        xs.append(i)
+        ys.append(int(valoreSensore))
+        xs = xs[-20:]
+        ys = ys[-20:]
+        ax.clear()
+        ax.plot(xs, ys, marker="o")
+        plt.title("Il grafico dei valori del sensore")
+        plt.xlabel("X - Secondi")
+        plt.ylabel("Y - Valori sensore")
+        plt.axes([0, i, 0, 1023])
     else:
         print("pacchetto scartato")
+
+ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=1000)
+plt.show()
+
 
