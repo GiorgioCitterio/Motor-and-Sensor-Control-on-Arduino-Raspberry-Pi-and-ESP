9e10abeb1d80cbec9633a2bb7cc6507a584dff12
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2022-12-28 16:53:07 +0100

    commit

--- 04_Python-4-GUI-C#/04_GUI_Csharpp/04_GUI_Csharpp/guiAttuatore1.xaml.cs
+++ 04_Python-4-GUI-C#/04_GUI_Csharpp/04_GUI_Csharpp/guiAttuatore1.xaml.cs
@@ -1,62 +1,63 @@
-//using System.IO.Ports;
-//using System.Text.Encodings;
-//namespace _04_GUI_Csharpp;
+using System.IO.Ports;
+namespace _04_GUI_Csharpp;
 
-//[StructLayout(LayoutKind.Explicit)]
-//public struct Pack
-//{
+public struct Pack
+{
+    public const string IDCORRETTO = "BE";
+    public const string MITTENTE = "M001";
+    public const string DESTINATARIO = "D031";
+    public const string TIPO = "A1";
+    public string direzione = "A";
+    public int velocita = 0;
+    public const string VUOTO = "----------------";
 
-//}
-//public partial class guiAttuatore1 : ContentPage
-//{
-//    static SerialPort serialPort = new SerialPort("COM3", 9600);
-//    static const byte[] IDCORRETTO = Encoding.ASCII.GetBytes("BE");
-//    static const byte[] MITTENTE = "M001";
-//    static const string DESTINATARIO = "D031";
-//    static const string TIPO = "A1";
-//    static byte direzione = "A";
-//    static byte velocita = 0;
-//    static const string VUOTO = "----------------";
-//    public guiAttuatore1()
-//    {
-//        InitializeComponent();
-//        Pack pack = new Pack();
-//    }
-//    private void Inc10(object sender, EventArgs e)
-//    {
-//        pack.velocita += 10;
-//        if (pack.velocita >= 250)
-//            pack.velocita = 250;
-//        else
-//            pack.velocita += 10;
-//        Velocità.Text = "Velocità: " + pack.velocita.ToString();
-//        serialPort.Open();
-//        serialPort.Write(pack);
-//        serialPort.Close();
-//        Console.WriteLine(pack);
-//    }
+    public Pack()
+    {
+    }
+}
+public partial class guiAttuatore1 : ContentPage
+{
+    static SerialPort serialPort = new SerialPort("COM3", 9600);
+    
+    public guiAttuatore1()
+    {
+        InitializeComponent();
+    }
+    private void Inc10(object sender, EventArgs e)
+    {
+        //pack.velocita += 10;
+        //if (pack.velocita >= 250)
+        //    pack.velocita = 250;
+        //else
+        //    pack.velocita += 10;
+        //Velocità.Text = "Velocità: " + pack.velocita.ToString();
+        //serialPort.Open();
+        //serialPort.Write(pack);
+        //serialPort.Close();
+        //Console.WriteLine(pack);
+    }
 
-//    private void Dec10(object sender, EventArgs e)
-//    {
-//        pack.velocita -= 10;
-//        if (pack.velocita >= 250)
-//            pack.velocita = 250;
-//        else
-//            pack.velocita -= 10;
-//        Velocità.Text = "Velocità: " + pack.velocita.ToString();
-//        serialPort.Open();
-//        serialPort.Write(pack);
-//        serialPort.Close();
-//        Console.WriteLine(pack);
-//    }
+    private void Dec10(object sender, EventArgs e)
+    {
+        //pack.velocita -= 10;
+        //if (pack.velocita >= 250)
+        //    pack.velocita = 250;
+        //else
+        //    pack.velocita -= 10;
+        //Velocità.Text = "Velocità: " + pack.velocita.ToString();
+        //serialPort.Open();
+        //serialPort.Write(pack);
+        //serialPort.Close();
+        //Console.WriteLine(pack);
+    }
 
-//    private void Avanti(object sender, EventArgs e)
-//    {
+    private void Avanti(object sender, EventArgs e)
+    {
 
-//    }
+    }
 
-//    private void Indietro(object sender, EventArgs e)
-//    {
+    private void Indietro(object sender, EventArgs e)
+    {
 
-//    }
-//}
+    }
+}
