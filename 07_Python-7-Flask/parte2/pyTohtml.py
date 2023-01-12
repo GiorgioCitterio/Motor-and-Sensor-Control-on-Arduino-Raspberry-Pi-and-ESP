534d3b57f18ebc9384572532aef341c7b9e8d7d1
umbecol <91663374+umbecol@users.noreply.github.com>
2023-01-12 09:35:55 +0100

    commit

--- 07_Python-7-Flask/parte2/pyTohtml.py
+++ 07_Python-7-Flask/parte2/pyTohtml.py
@@ -3,4 +3,11 @@ import shutil
 import json
 
 with open('05_Python-5-JSON/parte3/datiSensore.json', 'r') as fp:
-        lista2 = json.load(fp)
+        lista = json.load(fp)
+date = []
+valoriSensori = []
+for i in range(len(lista)):
+        date.append(lista[i]["DataOra"])
+        valoriSensori.append(lista[i]["Valore"])
+        stringa = "<table><tr><th>Data e ora</th><th>Valore</th></tr><tr><td>"+str(date[i])+"</td><td>"+str(valoriSensori[i])+"</td></tr></table>"
+        print(stringa)
