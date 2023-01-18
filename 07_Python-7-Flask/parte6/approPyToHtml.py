0abbb4639295525d4948de2fca68cd13ec3328ca
umbecol <91663374+umbecol@users.noreply.github.com>
2023-01-18 18:54:44 +0100

    commid

---
+++ 07_Python-7-Flask/parte6/approPyToHtml.py
@@ -0,0 +1,34 @@
+import json
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+app = dash.Dash(__name__)
+
+with open('05_Python-5-JSON/parte3/datiSensore.json', 'r') as fp:
+        lista = json.load(fp)
+date = []
+valoriSensori = []
+for i in range(len(lista)):
+        date.append(lista[i]["DataOra"])
+        valoriSensori.append(lista[i]["Valore"])
+
+df = pd.DataFrame({
+'DataOra': date,
+'Valore': valoriSensori})
+fig = px.bar(df, x='DataOra', y='Valore', barmode='group')
+
+app.layout = html.Div(children=[
+   html.H1(children='Valori sensore'),
+   html.Div(children='''
+   Ploty: Estratto file JSON.
+   '''),
+   dcc.Graph(
+      id='example-graph',
+      figure=fig
+   )
+]) 
+if __name__ == '__main__':
+   app.run_server(debug=True)
