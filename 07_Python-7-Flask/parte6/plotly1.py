f027721d9994f8dc2a8d9a498f75a8df16cadb33
umbecol <91663374+umbecol@users.noreply.github.com>
2023-01-19 08:35:53 +0100

    cozzid

--- 07_Python-7-Flask/parte6/plotly1.py
+++ 07_Python-7-Flask/parte6/plotly1.py
@@ -1,34 +1,37 @@
 import json
 import dash
+from flask import Flask
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
+import os
 
-app = dash.Dash(__name__)
+app = Flask(__name__)
+@app.route('/')
 
-with open('05_Python-5-JSON/parte3/datiSensore.json', 'r') as fp:
-        lista = json.load(fp)
-date = []
-valoriSensori = []
-for i in range(len(lista)):
-        date.append(lista[i]["DataOra"])
-        valoriSensori.append(lista[i]["Valore"])
-
-df = pd.DataFrame({
-'DataOra': date,
-'Valore': valoriSensori})
-fig = px.bar(df, x='DataOra', y='Valore', barmode='group')
-
-app.layout = html.Div(children=[
-   html.H1(children='Valori sensore'),
-   html.Div(children='''
-   Ploty: Estratto file JSON.
-   '''),
-   dcc.Graph(
-      id='example-graph',
-      figure=fig
-   )
-]) 
-if __name__ == '__main__':
-   app.run_server(debug=True)
+def funz():
+   app = dash.Dash(__name__)
+   path = os.getcwd()+'/datiSensore.json'
+   with open(path, 'r') as fp:
+         lista = json.load(fp)
+   date = []
+   valoriSensori = []
+   for i in range(len(lista)):
+         date.append(lista[i]["DataOra"])
+         valoriSensori.append(lista[i]["Valore"])
+   df = pd.DataFrame({
+   'DataOra': date,
+   'Valore': valoriSensori})
+   fig = px.bar(df, x='DataOra', y='Valore', barmode='group')
+   app.layout = html.Div(children=[
+      html.H1(children='Valori sensore'),
+      html.Div(children='''
+      Ploty: Estratto file JSON.
+      '''),
+      dcc.Graph(
+         id='example-graph',
+         figure=fig
+      )
+   ]) 
+   return "a"
