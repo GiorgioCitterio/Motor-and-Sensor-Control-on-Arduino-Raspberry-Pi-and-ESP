d64b621d5f0db551e4a2d301b5c0ba2069b86d34
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-01-19 08:45:15 +0100

    Update template1.py

---
+++ 07_Python-7-Flask/parte6/template1.py
@@ -0,0 +1,23 @@
+from flask import render_template
+from flask import Flask
+import json
+import os
+
+path = os.getcwd()+'/datiSensore.json'
+app = Flask(__name__)
+@app.route('/template/')
+@app.route('/template/<name>')
+
+def returnHtml(name = None):
+        with open(path, 'r') as fp:
+                lista = json.load(fp)
+        date = []
+        valoriSensori = []
+        stringa = "" 
+        for i in range(len(lista)):
+                date.append(lista[i]["DataOra"])
+                valoriSensori.append(lista[i]["Valore"])
+                stringa += "<tr><td>"+str(date[i])+"</td><td>"+str(valoriSensori[i])+"</td></tr>"
+        stringaFinale= "<style>table, th, td{border: 1px solid black;}</style><body><table><tr><th>Data e ora</th><th>Valore</th></tr>"+stringa+"</table></body>"
+        print(stringaFinale)
+        return render_template(stringaFinale, name=name)
