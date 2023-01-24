bce1880e7f58b9df69417b35105b6b07d1c4173c
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-01-24 10:48:43 +0100

    commit template

--- 07_Python-7-Flask/parte6/template1.py
+++ 07_Python-7-Flask/parte6/template1.py
@@ -5,18 +5,9 @@ import os
 
 path = os.getcwd()+'/datiSensore.json'
 app = Flask(__name__)
-@app.route('/<name>')
+@app.route('/')
 
-def returnHtml(name):
+def returnHtml():
         with open(path, 'r') as fp:
                 lista = json.load(fp)
-        date = []
-        valoriSensori = []
-        stringa = ""
-        for i in range(len(lista)):
-                date.append(lista[i]["DataOra"])
-                valoriSensori.append(lista[i]["Valore"])
-                stringa += "<tr><td>"+str(date[i])+"</td><td>"+str(valoriSensori[i])+"</td></tr>"
-        stringaFinale= "<!DOCTYPE html><html lang=en><head><meta charset=UTF-8><meta http-equiv=X-UA-Compatible content=IE=edge><meta name=viewport content=width=device-width, initial-scale=1.0><meta http-equiv=refresh content=1><title>Dati sensore</title><style>table, th, td{border: 1px solid black;}</style></head><body><table><tr><th>Data e ora</th><th>Valore</th></tr>"+stringa+"</table></body></html"
-        print(stringaFinale)
-        return render_template('index.html')
+        return render_template('index.html', dizValori=lista)
