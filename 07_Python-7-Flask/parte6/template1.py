220817adb78163390166378b3052d06e3987c9fa
umbecol <91663374+umbecol@users.noreply.github.com>
2023-01-24 10:59:28 +0100

    cospit

--- 07_Python-7-Flask/parte6/template1.py
+++ 07_Python-7-Flask/parte6/template1.py
@@ -3,11 +3,12 @@ from flask import Flask
 import json
 import os
 
-path = os.getcwd()+'/datiSensore.json'
+pathJ = os.getcwd()+'/datiSensore.json'
+pathH = os.getcwd()+'/index.html'
 app = Flask(__name__)
 @app.route('/')
 
 def returnHtml():
-        with open(path, 'r') as fp:
+        with open(pathJ, 'r') as fp:
                 lista = json.load(fp)
-        return render_template('index.html', dizValori=lista)
+        return render_template(pathH, dizValori=lista)
