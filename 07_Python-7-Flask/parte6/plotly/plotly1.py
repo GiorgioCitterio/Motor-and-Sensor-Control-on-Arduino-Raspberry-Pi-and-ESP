91b2cb92358ca7ba71f7b26d4a3a363fb9d875a8
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-01-26 09:22:39 +0100

    commit

--- 07_Python-7-Flask/parte6/plotly/plotly1.py
+++ 07_Python-7-Flask/parte6/plotly/plotly1.py
@@ -1,19 +1,15 @@
 import json
 import dash
-from flask import Flask, render_template
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 import os
-import plotly
 
-app = Flask(__name__)
-@app.route('/')
+app = dash.Dash(__name__)
+path = os.getcwd()+'/datiSensore.json'
 
 def funz():
-   app = dash.Dash(__name__)
-   path = os.getcwd()+'/datiSensore.json'
    with open(path, 'r') as fp:
          lista = json.load(fp)
    date = []
@@ -25,5 +21,16 @@ def funz():
    'DataOra': date,
    'Valore': valoriSensori})
    fig = px.bar(df, x='DataOra', y='Valore', barmode='group')
-   graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-   return render_template('index.html', graphJSON=graphJSON)
+   app.layout = html.Div(children=[
+      html.H1(children='Valori sensore'),
+      html.Div(children='''
+      Ploty: Estratto file JSON.
+      '''),
+      dcc.Graph(
+         id='example-graph',
+         figure=fig
+      )
+   ])
+
+if __name__ == '__main__':
+   app.run_server(debug=True)
