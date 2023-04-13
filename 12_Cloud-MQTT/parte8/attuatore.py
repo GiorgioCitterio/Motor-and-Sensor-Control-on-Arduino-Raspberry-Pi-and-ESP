5ec314cbbb81cfe65a4be160968423d83a457ef4
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-04-13 09:50:28 +0200

    parte 8

---
+++ 12_Cloud-MQTT/parte8/attuatore.py
@@ -0,0 +1,68 @@
+import paho.mqtt.client as client
+from flask import Flask, request, render_template, redirect
+import struct
+import time
+import json
+import sys
+import pigpio
+from nrf24 import *
+
+#costanti rf24
+PIGPIONAME='localhost'
+PIGPIOPORT=8888
+WRITINGPIPE='00001'
+
+#costanti motore
+ID=b"BE"
+MITTENTE=b"P001"
+DESTINATARIO=b"A001"
+TIPO=b"A1"
+VUOTO=("-"*16).encode()
+
+#costanti mqtt
+TOPIC="tps/motoreAtt"
+BROKER = "172.17.4.29"
+
+lista = []
+direzione = b"A"
+app = Flask(__name__)
+
+# connessione a pigpiod
+pi = pigpio.pi(PIGPIONAME, PIGPIOPORT) 
+if not pi.connected: 
+    print("Pigpiod non connesso. Lanciare: SUDO PIGPIOD") 
+    sys.exit() 
+ 
+# Crea l'oggetto NRF24  
+nrf = NRF24(pi, ce=17, payload_size=32, channel=76,data_rate=RF24_DATA_RATE.RATE_2MBPS, pa_level=RF24_PA.LOW) 
+ 
+# apre le pipe 
+nrf.set_address_bytes(5) 
+nrf.open_writing_pipe(WRITINGPIPE)
+
+def on_connect(subscriber, userdata, flags, rc):
+    print("Connesso con return code" + str(rc))
+
+    subscriber.subscribe(TOPIC)
+
+def on_message(subscriber, userdata, msg):
+    #print(msg.topic+" "+str(msg.payload))
+    data = json.loads(msg.payload)
+    print(data)
+    dir = data["direzione"]
+    direzione = str(dir).encode()
+    val = data["velocita"]
+    v = str(val).zfill(3).encode()
+    msg=struct.pack("2s 4s 4s 2s 1s 3s 16s",ID,MITTENTE,DESTINATARIO,TIPO,direzione,v,VUOTO)
+    nrf.send(msg)
+    print(msg)
+    nrf.wait_until_sent()
+    nrf.power_up_rx() 
+
+subscriber = client.Client()
+subscriber.on_connect = on_connect
+subscriber.on_message = on_message
+
+subscriber.connect(BROKER, 1883, 60)
+
+subscriber.loop_forever()
---
+++ 12_Cloud-MQTT/parte8/sensore.py
@@ -0,0 +1,52 @@
+import paho.mqtt.publish as publish
+import time
+import time
+import sys
+import struct
+import json
+import pigpio
+from nrf24 import *
+
+#costanti mqtt
+TOPIC = "tps/sensorePotenza"
+BROKER = "172.17.4.29"
+
+#costanti sensore
+PIGPIONAME='localhost'
+PIGPIOPORT=8888
+READINGPIPE='00001'
+IDCORRETTO='BE'
+DESTINATARIOCORRETTO='P001'
+MIO_TIPO='S1'
+
+# connessione a pigpiod
+pi = pigpio.pi(PIGPIONAME, PIGPIOPORT)
+if not pi.connected:
+    print("Pigpiod non connesso. Lanciare: SUDO PIGPIOD")
+    sys.exit()
+
+# Crea l'oggetto NRF24
+nrf = NRF24(pi, ce=17, payload_size=32, channel=76,data_rate=RF24_DATA_RATE.RATE_2MBPS, pa_level=RF24_PA.LOW)
+
+# apre la pipe
+nrf.set_address_bytes(5)
+nrf.open_reading_pipe(RF24_RX_ADDR.P1, READINGPIPE)
+
+#lettura pacchetto 32 byte
+while True:
+    if nrf.data_ready():
+        pack=(struct.unpack("2s 4s 4s 2s 4s 16s",nrf.get_payload()))
+        id=pack[0].decode()
+        mittente=pack[1].decode()
+        destinatario=pack[2].decode()
+        tipo=pack[3].decode()
+        valoreSensore=pack[4].decode()
+        vuoto=pack[5].decode()
+        if ((id==IDCORRETTO)and(destinatario==DESTINATARIOCORRETTO)):
+            print(pack)
+            print("id e destinatario corretti e valoreSensore = " + valoreSensore)
+            s=int(valoreSensore)
+            dataOra = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+            dizionario = {'DataOra': dataOra, 'Valore' : s}
+            data = json.dumps(dizionario)
+            publish.single(TOPIC, data, hostname=BROKER)
